068ae62b7903316e8758141d5271726e79a45e07
Tomas13d <tomas.demobio@gmail.com>
2023-02-19 18:27:20 -0300

    feat: some details

--- src/App.jsx
+++ src/App.jsx
@@ -4,43 +4,30 @@ import Login from "./components/Login";
 import { AuthProvider } from "./contexts/AuthContext";
 import WithPrivateRoute from "./components/PrivateRoute/privateRoute";
 import Home from "./components/Home";
-import { useEffect } from "react";
+import { useEffect, useLayoutEffect } from "react";
 import auth from "./config/firebase";
 import "./App.css";
 
 function App() {
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const user = auth.currentUser;
-        const token = user && (await user.getIdToken());
-        console.log("token ->", token);
-        const payloadHeader = {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        };
-        const res = await fetch("http://localhost:3001", payloadHeader);
-        console.log(await res.text());
-      } catch (e) {
-        console.log(e);
-      }
+  useLayoutEffect(() => {
+    const generateToken = async () => {
+      const user = auth.currentUser;
+      const token = user && (await user.getIdToken());
+      window.localStorage.setItem('Token', token)
     };
-
-    fetchData();
-}, []);
+    generateToken();
+  }, []);
   return (
     <AuthProvider>
       <Routes>
-      <Route
-            path="/"
-            element={
-              <WithPrivateRoute>
-                <Home />
-              </WithPrivateRoute>
-            }
-          />
+        <Route
+          path="/"
+          element={
+            <WithPrivateRoute>
+              <Home />
+            </WithPrivateRoute>
+          }
+        />
         <Route path="/signup" element={<SignIn />} />
         <Route path="/login" element={<Login />} />
       </Routes>
